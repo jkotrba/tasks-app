7383352ce9b4389a8b323b828af98413b5bb02e6
jkotrba <jerome.kotrba@gmail>
2018-05-10 05:55:29 -0700

    add get all and post

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,7 +1,63 @@
 import React from 'react';
+import { Button, FormGroup, ControlLabel, FormControl } from 'react-bootstrap';
 
 class AddTask extends React.Component {
+  constructor() {
+    super();
 
+    this.state = { title: '', description: '' };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    const target = e.target;
+    const name = target.name;
+
+    this.setState({
+      [name]: target.value
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const task = this.state;
+
+    if(this.props.onAdd) {
+      this.props.onAdd(task);
+    }
+    this.setState({ title: '', description: ''});
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <FormGroup>
+            <ControlLabel htmlFor="title">Title</ControlLabel>
+            <FormControl
+              type="text"
+              name="title"
+              id="title"
+              value={this.state.title}
+              onChange={this.handleChange}
+            />
+          </FormGroup>
+          <FormGroup>
+          <ControlLabel htmlFor="description">Description</ControlLabel>
+          <FormControl
+            componentClass="textarea"
+            name="description"
+            id="description"
+            value={this.state.description}
+            onChange={this.handleChange}
+          />
+          </FormGroup>
+          <Button type="submit" bsStyle="primary">Add New Task</Button>
+        </form>
+      </div>
+    )
+  }
 }
 
 export default AddTask;
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,18 +1,69 @@
 import React from 'react';
+import { Table } from 'react-bootstrap';
+import '../css/task-list.css';
+import AddTask from './AddTask';
 
 class TaskList extends React.Component {
   constructor() {
     super();
 
-    this.state = { tasks: []};
+    this.state = { tasks: [] };
+    this.onAddTask = this.onAddTask.bind(this);
+  }
+
+  getTasks() {
+    fetch('http://localhost:3001/api/tasks')
+      .then(response => response.json())
+      .then(json => {
+        this.setState({ tasks: json.data });
+      });
+  }
+
+  componentDidMount() {
+    this.getTasks();
+  }
+
+  onAddTask(task) {
+    const payload = JSON.stringify({ data: task });
+    fetch('http://localhost:3001/api/tasks', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: payload
+    }).then(() => this.getTasks());
   }
 
   render() {
     return (
-      <div>
+      <div className="task-list">
         <h2>Tasks</h2>
+        <Table className="striped">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Description</th>
+              <th>Created</th>
+              <th>Completed</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.tasks.map(t => {
+              return (
+                <tr key={t.taskId}>
+                  <td>{t.title}</td>
+                  <td>{t.description}</td>
+                  <td>{t.createdDate}</td>
+                  <td>{t.completedDate}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </Table>
+        <AddTask onAdd={this.onAddTask} />
       </div>
-    )
+    );
   }
 }
 
